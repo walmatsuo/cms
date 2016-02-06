4983a112fb25f15d873d0c59b0cd89879e41abec
walmatsuo <walmatsuo@gmail.com>
2016-02-05 22:51:21 -0200

    Atualizando nome BD (cm2)

--- src/java/br/cms/BD/ConectaBanco.java
+++ src/java/br/cms/BD/ConectaBanco.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConectaBanco {
 
     private static final String DRIVER = "org.postgresql.Driver";
-    private static final String URL = "jdbc:postgresql://localhost:5432/cms";
+    private static final String URL = "jdbc:postgresql://localhost:5432/cm2";
     private static final String USUARIO = "postgres";
     private static final String SENHA = "postgres";
 
